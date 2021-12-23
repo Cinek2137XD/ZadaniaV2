d65640abb652f8d78b90d3c0192af1b5849e3c06
Cinek2137XD <s25667@pjwstk.edu.pl>
2021-12-23 17:47:08 +0100

    23.12.2021

--- zad_2_5.cpp
+++ zad_2_5.cpp
@@ -4,7 +4,31 @@
 
 std::vector<std::string> split(std::string wejscie)
 {
-    std::string spacja = " ";
+    char spacja =' ';
     std::vector<std::string> slowa;
-    
+    std::string do_vec;
+    for (int i = 0; i < wejscie.size(); i++)
+    {
+        if(wejscie.at(i) != spacja)
+        {
+            do_vec += wejscie.at(i);
+        }
+        else if(do_vec.size()>= 0)
+        {
+            slowa.push_back(do_vec);
+            do_vec ="";
+        }
+    }
+    return slowa;
+}
+
+int main()
+{
+    std::string tekst = "test programu  dwie spacje przed";
+    std::vector<std::string> wyjscie = split(tekst);
+    for (int i = 0; i < wyjscie.size(); i++)
+    {
+        std::cout<<wyjscie[i]<<" ";
+    }
+    return 0;
 }
