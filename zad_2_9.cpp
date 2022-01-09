eb32c86d20186e9ff445c7075598354b2ccea42b
Cinek2137XD <s25667@pjwstk.edu.pl>
2022-01-09 19:43:07 +0100

    działa

--- zad_2_9.cpp
+++ zad_2_9.cpp
@@ -5,17 +5,38 @@
 bool contains_all(std::vector<int>& dUpa, std::vector<int>& kys)
 {
     int testowana;
-    bool wynik = false;
+    std::vector<int> ok;
     for(int i = 0; i < dUpa.size(); i++)
     {
         for(int x = 0; x < kys.size(); x++)
-        {
-            
-            if(x+1 == kys.size())
+        {            
+            if(dUpa[i] == kys[x])
             {
-                
+                ok.push_back(kys[x]);
             }
         }
     }
+    if(dUpa.size() == ok.size())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    std::vector<int> a {1,2,3,3,4,4,1,2}, b {1,2,3,4};
+    if(contains_all(a,b))
+    {
+        std::cout << "Zawiera";
+    }
+    else
+    {
+        std::cout << "Nie zawiera";
+    }
+    return 0;
 }
 
